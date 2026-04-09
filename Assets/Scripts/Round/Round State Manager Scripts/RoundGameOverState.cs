ec8579123480e7b79fb0ac3c1575fc94345f4222
Sam Lucarelli <nuckname13@gmail.com>
2026-04-09 12:45:02 +1000

    remove score board

--- Assets/Scripts/Round/Round State Manager Scripts/RoundGameOverState.cs
+++ Assets/Scripts/Round/Round State Manager Scripts/RoundGameOverState.cs
@@ -3,27 +3,16 @@ using UnityEngine;
 
 public class RoundGameOverState : RoundBaseState
 {
+    // When we reach the amount of rounds we call this to go to next level. can have multiple rounds a map.
     private TempScoreboard scoreboard;
     public override void EnterState(RoundStateManager manager)
     {
         Debug.Log("Round Over. Threshold Reached.");
         Debug.Log("Seen: " + RoundStateManager.AmountOfTimesPlayerSpottedByGuards);
-        
-       // manager.timer.isPaused = true;
 
-        // Find the temp scoreboard in the scene and activate it
-        /*
-        scoreboard = Object.FindObjectOfType<TempScoreboard>();
-        if (scoreboard != null)
-        {
-            scoreboard.isGameOver = true;
-        }
-        else
-        {
-            Debug.LogWarning("TempScoreboard script not found in the scene!");
-        }
-        */
-        // Lets just tp the player for now
+        // Player also needs to enter?
+        // Then we can switch back to About to start state.
+        manager.currentRoundIndex++;
     }
 
     public override void UpdateState(RoundStateManager manager)
